4257fade916d4c1c756b6af8508a346dbaddafd1
hena-ndombele <henandombele2021@gmail.com>
2024-03-29 11:31:26 +0100

    Ajout des routes pays et partenaire

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PaysController;
+use App\Http\Controllers\PartenaireController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +18,22 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
+//route pays
 Route::get('/pays', [PaysController::class,'index'])->name('pays.index');
+Route::post('/store', [PaysController::class,'store'])->name('store');
+//fin route pays
+
+
+//route partenaire
+Route::get('/partenaire', [PartenaireController::class,'index'])->name('partenaires.index');
+Route::post('/store', [PartenaireController::class,'store'])->name('store');
+Route::get('/partenaire-delete/{id}', [PartenaireController::class,'destroy'])->name('partenaires.destroy');
+//fin route partenaire
+
+
+
 
 require __DIR__.'/auth.php';
