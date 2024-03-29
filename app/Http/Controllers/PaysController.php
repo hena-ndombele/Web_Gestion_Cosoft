c88c690e8e9ad27d8eb3e2d41dd7899ea2d370d9
hena-ndombele <henandombele2021@gmail.com>
2024-03-29 11:29:39 +0100

    Modification du PaysController

--- app/Http/Controllers/PaysController.php
+++ app/Http/Controllers/PaysController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pays;
 
 class PaysController extends Controller
 {
     public function index(){
-        return view('pays.index');
+        $pays=Pays::all();
+        $paysCount=Pays::count();
+        $number=1;
+        return view('pays.index', compact('pays', 'number'));
+    }
+
+
+    public function store(Request $request){
+        $request->validate([
+            'pays'=>'required|unique:pays'
+        ]);
+
+
+        Pays::create([
+            'pays'=>$request->pays
+        ]);
+        return redirect()->route('pays.index')->with('success', 'Création du pays avec sucess');
     }
 }
